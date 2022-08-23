aea273b03781d85ef2d444aac3542f94a5edacec
JaneWnag <whjun3@gmail.com>
2022-08-23 09:02:21 +0800

    4 files modified, 4 added

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent {
-
-  constructor() { }
-
+  items = this.cartService.getItems();
+  constructor(private cartService: CartService) {}
 }
